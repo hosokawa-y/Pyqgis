79191641fc94faeb06901fcfefa49b3de48385eb
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2021-10-28 16:45:05 +0900

    指定したフォルダからベクタレイヤを追加

---
+++ add_vector_layer.py
@@ -0,0 +1,9 @@
+import glob
+
+s = "/Users/hosokawa/Map data/gyouseikuiki/kanto/N03-20210101_14_GML_kanagawa/vector/"
+files = glob.glob(s + "*.shp")
+for file in files:
+    print(file)
+    file_name = file.replace(s, "")
+    print(file_name)
+    iface.addVectorLayer(file, "", 'ogr')
